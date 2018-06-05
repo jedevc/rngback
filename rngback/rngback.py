f88388ac78976e78304b3165dbc2093fe17aca92
Justin Chadwell <jedevc@gmail.com>
2018-06-05 17:37:02 +0545

    Improve command line argument parsing and help text

--- rngback/rngback.py
+++ rngback/rngback.py
@@ -7,42 +7,53 @@ def main():
     Simple wrapper for the generator that parses command line arguments.
     '''
 
-    parser = argparse.ArgumentParser(prog='rngback',
-              description='Randomly generate visually pleasing geometric backgrounds.')
-    parser.add_argument('width', type=int, help='image width')
-    parser.add_argument('height', type=int, help='image width')
-    parser.add_argument('columns', type=int, help='number of columns')
-    parser.add_argument('rows', type=int, help='number of rows')
+    # create parser
+    name = 'rngback'
+    description = 'Randomly generate visually pleasing backgrounds.'
+    parser = argparse.ArgumentParser(prog=name, description=description)
 
-    parser.add_argument('-off', '--offset', default=0, type=int,
-              help='individual shape offset')
+    # define arguments
+    size = parser.add_argument_group('size')
+    size.add_argument('width', type=int, help='image width')
+    size.add_argument('height', type=int, help='image width')
+    size.add_argument('columns', type=int, help='number of columns')
+    size.add_argument('rows', type=int, help='number of rows')
 
-    parser.add_argument('-bg', '--background', default='white',
-              help='background color')
-    parser.add_argument('-fg', '--foreground', action='append',
-              help='foreground color')
+    generation = parser.add_argument_group('generation')
+    generation.add_argument('-s', '--seed', help='random generation seed')
+    generation.add_argument('-o', '--output', default='', help='output file')
 
-    parser.add_argument('-hvar', '--hue-variation', default=0, type=int,
-              help='foreground hue variation amount')
-    parser.add_argument('-svar', '--sat-variation', default=0, type=int,
-              help='foreground saturation variation amount')
-    parser.add_argument('-lvar', '--lit-variation', default=0, type=int,
-              help='foreground lightness variation amount')
+    colors = parser.add_argument_group('colors')
+    colors.add_argument('-bg', '--background', default='white',
+            help='background color')
+    colors.add_argument('-fg', '--foreground', action='append',
+            help='foreground color')
 
-    parser.add_argument('-s', '--seed', help='random generation seed')
+    variation = parser.add_argument_group('variation')
+    variation.add_argument('-hvar', '--hue-variation', default=0, type=int,
+            help='foreground hue variation amount')
+    variation.add_argument('-svar', '--sat-variation', default=0, type=int,
+            help='foreground saturation variation amount')
+    variation.add_argument('-lvar', '--lit-variation', default=0, type=int,
+            help='foreground lightness variation amount')
 
-    parser.add_argument('-o', '--output', default='', help='output file')
+    visual = parser.add_argument_group('visual')
+    visual.add_argument('-off', '--offset', default=0, type=int,
+            help='individual shape offset')
 
+    # parse arguments
     args = parser.parse_args()
     if args.foreground is None:
         args.foreground = 'black'
 
     variation = (args.hue_variation, args.sat_variation, args.lit_variation)
 
+    # generate background
     gen = Generator(args.width, args.height, args.columns, args.rows,
             args.offset, args.background, args.foreground, variation)
     img = gen.generate(args.seed)
 
+    # save background
     if args.output:
         img.save(args.output)
     else:
